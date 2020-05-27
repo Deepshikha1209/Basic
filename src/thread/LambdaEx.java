78215eaeaa1aa29e5ded39541177fb33dbab56d6
Deepshikha Yadav <deepshikha.cs16@gmail.com>
2020-05-27 14:33:35 +0530

    Adding third variable

--- src/thread/LambdaEx.java
+++ src/thread/LambdaEx.java
@@ -3,18 +3,19 @@ interface MathInt {
 	public int calculate(int a, int b);
 }
 
-MathInt add = (a, b) -> {
-	return a + b;
+MathInt add = (a, b,c) -> {
+	return a + b+c;
 };
-MathInt multiply = (a, b) -> {
-	return a * b;
+MathInt multiply = (a, b,c) -> {
+	return a * b*c;
 };
-MathInt subtract = (a, b) -> {
-	return a - b;
+MathInt subtract = (a, b,c) -> {
+	return a - b-c;
 };
 
 int a = 10;
 int b = 5;
-System.out.println("Add " + add.calculate(a, b));
-System.out.println("Multiply " + multiply.calculate(a, b));
-System.out.println("Suntract " + subtract.calculate(a, b));
+int c=1;
+System.out.println("Add " + add.calculate(a, b,c));
+System.out.println("Multiply " + multiply.calculate(a, b,c));
+System.out.println("Suntract " + subtract.calculate(a, b,c));
